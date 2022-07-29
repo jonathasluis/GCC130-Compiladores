01d74df1689ad59b752e9a2f7d1ff9b831a4e8fb
jonathas-sousa <jonathas@scadiagro.com.br>
2022-07-29 19:27:46 -0300

    Correção erros e melhorias
    - correção comentarios
    - correcao texto
    - alteração if else then

--- AnalisadorLexico/src/ExemploLexer.java
+++ AnalisadorLexico/src/ExemploLexer.java
@@ -3,15 +3,16 @@ import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.Token;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExemploLexer {
 
     public static void main(String[] args) {
         List<Integer> linhas = new ArrayList<Integer>();
-
-        //String filename = "AnalisadorLexico/src/codigo.txt";
-        String filename = "AnalisadorLexico/src/codigoErro.txt";
+        String filename = "AnalisadorLexico/src/codigo.txt";
+        //String filename = "AnalisadorLexico/src/codigoErro.txt";
+        System.out.println("Token: <\"Lexema\",\"Classe\">\n");
         try {
             CharStream input = CharStreams.fromFileName(filename);
             MinhaGramaticaLexer lexer = new MinhaGramaticaLexer(input);
@@ -19,19 +20,20 @@ public class ExemploLexer {
             while (!lexer._hitEOF) {
                 token = lexer.nextToken();
                 if (!lexer.getVocabulary().getDisplayName(token.getType()).equalsIgnoreCase("ErrorChar")) {
-                    System.out.println("Token: " + token.toString());
-                    System.out.println("    Lexema: " + token.getText());
-                    System.out.println("    Classe: " + lexer.getVocabulary().getDisplayName(token.getType()));
+                    System.out.println("Token: <\""
+                            + token.getText()
+                            + "\",\""
+                            + lexer.getVocabulary().getDisplayName(token.getType())
+                            + "\">");
                 } else {
-                    if(!linhas.contains(token.getLine()))
+                    if (!linhas.contains(token.getLine())) {
                         linhas.add(token.getLine());
+                    }
                 }
             }
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-        System.out.println("\nErros nas linhas: "+linhas);
+        System.out.println("\nErros nas linhas: " + linhas);
     }
 }
